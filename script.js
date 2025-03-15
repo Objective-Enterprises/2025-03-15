ae43e7301aea95800f3622e1d9dd8f8bd8311b0a
David Y. Stephenson <david@davidystephenson.com>
2025-03-15 12:54:12 -0400

    Working search

--- script.js
+++ script.js
@@ -4,17 +4,19 @@ const movieList = document.getElementById('movie-list')
 
 console.log('movies', movies)
 
-const cards = movies.map(movie => {
-  return `
-    <div class='movie-card'> 
-      <h2>${movie.title}</h2>
-      <img src="${movie.image}" style="width: 100%" />
-      <div>Genre: ${movie.genre}</div>
-      <div>Director: ${movie.director}</div>
-    </div>
-  `
-})
-movieList.innerHTML = cards
+function displayCards (movies) {
+  const cards = movies.map(movie => {
+    return `
+      <div class='movie-card'> 
+        <h2>${movie.title}</h2>
+        <img src="${movie.image}" style="width: 100%" />
+        <div>Genre: ${movie.genre}</div>
+        <div>Director: ${movie.director}</div>
+      </div>
+    `
+  })
+  movieList.innerHTML = cards
+}
 
 search.addEventListener('input', (event) => {
   const lowerValue = event.target.value.toLowerCase()
@@ -26,5 +28,7 @@ search.addEventListener('input', (event) => {
     })
     return match
   })
-  console.log('filtered', filtered)
+  displayCards(filtered)
 })
+
+displayCards(movies)
